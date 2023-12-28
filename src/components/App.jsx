900054381da55ca28420253d537d70ae5817b76e
LucreciaUA <118127063+LucreciaUA@users.noreply.github.com>
2023-12-28 21:52:45 +0200

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Phonebook from "./phonebook";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        
+        alignItems: 'center',
+        color: '#010101'
+      }}
+    >
+      <Phonebook/>
+    </div>
+  );
+};
